2d855ca19b4cdd5b804a533d963ecc293146024c
Cristi Minica <cristi.minica@caphyon.com>
2019-04-23 14:46:03 +0300

    Add vue cli pwa plugin.

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
