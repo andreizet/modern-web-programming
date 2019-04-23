2a825533a50df3a7b022e24a5b9f40119c086d87
Cristi Minica <cristi.minica@caphyon.com>
2019-04-23 14:54:40 +0300

    Add new pwa build.

---
+++ vue-cli/dist/precache-manifest.3d6f5420086d3c1f70f35ab6cebddd6c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3804fa86a12e6edbef245e98bad5cd05",
+    "url": "/img/harvest.3804fa86.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "20384c8705e401082f06a82419422206",
+    "url": "/img/plus.20384c87.svg"
+  },
+  {
+    "revision": "87c6927e2c030a7003a6",
+    "url": "/css/app.d3385074.css"
+  },
+  {
+    "revision": "d16fdff0c7a149ddc23a",
+    "url": "/css/chunk-vendors.1fe18d50.css"
+  },
+  {
+    "revision": "d16fdff0c7a149ddc23a",
+    "url": "/js/chunk-vendors.ef68b4e4.js"
+  },
+  {
+    "revision": "e49ffb54122ed084a196c5ffbf8382ae",
+    "url": "/img/plate.e49ffb54.svg"
+  },
+  {
+    "revision": "8f0c6230114dbc1ae37bc93c9b21007f",
+    "url": "/img/menu.8f0c6230.svg"
+  },
+  {
+    "revision": "3f18ddfed6084d04fa7c42c0c208cbc1",
+    "url": "/img/loading.3f18ddfe.svg"
+  },
+  {
+    "revision": "c396fbb02a8cba06d902",
+    "url": "/js/about.45b81ded.js"
+  },
+  {
+    "revision": "87c6927e2c030a7003a6",
+    "url": "/js/app.8a445e8d.js"
+  },
+  {
+    "revision": "b64bf13971aebf584cc784b9432aa24e",
+    "url": "/img/logo.b64bf139.svg"
+  },
+  {
+    "revision": "a7e469fb2ebba73e56edcb6dd1d2f865",
+    "url": "/img/book.a7e469fb.svg"
+  },
+  {
+    "revision": "e06ee095513edc3632c071771c1b757e",
+    "url": "/img/minus.e06ee095.svg"
+  },
+  {
+    "revision": "3483f8dcbf8f88329ef7567520fbab0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37ae0cb71aa9b2b7d597b565be134558",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "c396fbb02a8cba06d902",
+    "url": "/css/about.af2b3c85.css"
+  },
+  {
+    "revision": "d68738b98a420cb1b1d285b8d2a5d6c1",
+    "url": "/README.md"
+  }
+];
